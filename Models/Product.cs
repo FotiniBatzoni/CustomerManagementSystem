dc54ceeff947bf434311938ea2540e9467448de0
fotini <f.batzoni@gmail.com>
2023-02-01 12:07:06 +0200

    Creating Product

---
+++ Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace CustomerManagementSystem.Models
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string ProductName { get; set; }
+        public string ProductDescription { get; set; }
+
+        public float Price { get; set; }
+
+
+    }
+}
