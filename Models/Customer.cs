d86a8627f99f760ed88db493bd01a2b6a2d56d63
fotini <f.batzoni@gmail.com>
2023-02-01 12:05:00 +0200

    Creating Customer

---
+++ Models/Customer.cs
@@ -0,0 +1,15 @@
+﻿namespace CustomerManagementSystem.Models
+{
+    public class Customer
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+        public string Email { get; set; }
+
+        public string HomeAddress { get; set; }
+
+        public string WorkAddress { get; set; }
+
+    }
+}
