c7448222ec4fb574e8e36f247c0f3a82480ad79a
fotini <f.batzoni@gmail.com>
2023-02-01 12:09:08 +0200

    Creating OrderItem

---
+++ Models/OrderItem.cs
@@ -0,0 +1,11 @@
+﻿namespace CustomerManagementSystem.Models
+{
+    public class OrderItem
+    {
+        public Guid OrderId { get; set; }
+
+        public float Quantity { get; set; }
+
+        public decimal PurchasePrice { get; set; }
+    }
+}
