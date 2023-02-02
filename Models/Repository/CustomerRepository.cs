1e9d58b2fd20e6b87a6c362dcba25ade38e142d6
fotini <f.batzoni@gmail.com>
2023-02-02 13:44:43 +0200

    Adding methods to repository customer

---
+++ Models/Repository/CustomerRepository.cs
@@ -0,0 +1,26 @@
+﻿namespace CustomerManagementSystem.Models.Repository
+{
+    public class CustomerRepository
+    {
+        public Customer Retrieve(Guid customerId)
+        {
+            Customer customer = new Customer(customerId);
+
+            if(Guid.TryParse("a5b354d2-e019-4daa-ae46-15ea150ac5bd", out Guid parsedGuid))
+            {
+                customer.CustomerId = parsedGuid;
+                customer.FirstName = "Tony";
+                customer.LastName = "Balis";
+                customer.Email = "tony@example.com";
+
+            }
+
+            return customer;
+        }
+
+        public bool Save (Customer customer)
+        {
+            return true;
+        }
+    }
+}
