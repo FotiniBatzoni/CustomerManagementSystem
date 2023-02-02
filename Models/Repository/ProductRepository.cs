810ba4113aee3c2a9f05ca380634f44aab5e9e0f
fotini <f.batzoni@gmail.com>
2023-02-02 17:01:52 +0200

    Adding ProductRepository  and Test

---
+++ Models/Repository/ProductRepository.cs
@@ -0,0 +1,25 @@
+﻿namespace CustomerManagementSystem.Models.Repository
+{
+    public class ProductRepository
+    {
+        public Product Retrieve(Guid productId)
+        {
+            Product product = new Product(productId);
+
+            if(Guid.TryParse("1A3B944E-3632-467B-A53A-206305310BAB", out Guid parsedGuid))
+            {
+                product.ProductId = parsedGuid;
+                product.ProductName = "Gum";
+                product.ProductDescription = "lorem ipsum";
+                product.CurrentPrice = (decimal?)0.45;
+            }
+
+            return product;
+        }
+
+        public bool Save (Product product)
+        {
+            return true;
+        }
+    }
+}
