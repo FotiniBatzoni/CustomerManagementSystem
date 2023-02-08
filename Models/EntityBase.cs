ace5e894fcd26f02be51ed9a56294535c1c2e863
fotini <f.batzoni@gmail.com>
2023-02-08 13:28:36 +0200

    Adding abstract EntityBase Class

---
+++ Models/EntityBase.cs
@@ -0,0 +1,23 @@
+﻿namespace CustomerManagementSystem.Models
+{
+    public enum EntityStateOption
+    {
+        Active,
+        Deleted
+    }
+
+    public abstract class EntityBase
+    {
+        public bool IsNew { get; private set; }
+        
+        public bool HasChanges { get; set; }
+
+        public bool IsValid
+        {
+            get { return true; }
+        }
+  
+        public EntityStateOption EntityState { get; set; }
+
+    }
+}
